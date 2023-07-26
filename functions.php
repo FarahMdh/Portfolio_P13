772e1cb39aae00c840d8ec9970b76583dfe13878
Farah Mdh <mdhfarah3@gmail.com>
2023-07-27 00:38:03 +0200

    Ajout de la modal de contact : style & JS

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
     
     function theme_enqueue_styles() {
         wp_enqueue_style( 'style', get_stylesheet_directory_uri() .'/style.css' );   
-        wp_enqueue_script( 'portfolio', get_stylesheet_directory_uri() .'/script.js', array(), '1.0.0', true );
+        wp_enqueue_script( 'script', get_stylesheet_directory_uri() .'/script.js', array(), '1.0.0', true );
         }
-        
+
     add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
--- templates_parts/contact-modal.php
+++ templates_parts/contact-modal.php
@@ -3,7 +3,9 @@
   <div class="overlay modal-trigger"></div>
   
   <div class="modal">
-    <h1>Work with me!</h1>
+    <div class="modal-title">
+      <h1>Work with me!</h1>
+    </div>
     <?php
 		  echo do_shortcode('[contact-form-7 id="9" title="Formulaire de contact"]');
 	?>
